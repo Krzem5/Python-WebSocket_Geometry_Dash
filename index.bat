7a46fa9ae1f34a8e2fdf6e17da4d177ed5853359
Krzem <krzem5.dev@gmail.com>
2021-01-01 12:43:01 +0100

    Push Update 01/01/2021, 11:54:24

---
+++ index.bat
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+@echo off
+python startup.py
+=======
+@echo off
+cls
+python src/startup.py
+>>>>>>> 4b6e43a (Push Update 01/12/2021, 20:03:43)
---
+++ ws_start.bat
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+@echo off
+cls
+python WSServer.py
+=======
+@echo off
+cls
+python src/WSServer.py
+>>>>>>> 4b6e43a (Push Update 01/12/2021, 20:03:43)
