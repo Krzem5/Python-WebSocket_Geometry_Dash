06821ba3a328e6714a5b0e3af0be9fa7bea22797
Krzem <krzem5.dev@gmail.com>
2021-04-08 09:22:34 +0200

    Push Update 04/08/2021, 09:22:20

--- build.bat
+++ build.bat
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-@echo off
-python startup.py
-=======
-=======
->>>>>>> f6398e4 (Push Update 04/06/2021, 16:28:55)
 @echo off
 cls
 python src/startup.py
--- ws_start.bat
+++ ws_start.bat
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-@echo off
-cls
-python WSServer.py
-=======
-=======
->>>>>>> f6398e4 (Push Update 04/06/2021, 16:28:55)
 @echo off
 cls
 python src/WSServer.py
